9570ac463282d312af85e3005fd0144f09c6ebf8
Qin Yuzhen <1@jin-dan.site>
2022-11-15 08:57:45 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
-sudo apt install curl git python3 build-essential pkg-config -y
-sudo apt install cmake python3-pil libusb-1.0-0-dev libsdl2-dev libudisks2-dev libglib2.0-dev -y
+sudo apt update
+sudo apt install curl git python3 build-essential pkg-config cmake python3-pil libusb-1.0-0-dev libsdl2-dev libudisks2-dev libglib2.0-dev -y
+
 echo "export PATH=\$PATH:~/.local/bin" >> ~/.profile
 source ~/.profile
-curl "https://gitea.planet-casio.com/Lephenixnoir/GiteaPC/raw/branch/master/install.sh" -o /tmp/giteapc-install.sh && bash /tmp/giteapc-install.sh
-giteapc install Lephenixnoir/fxsdk Lephenixnoir/sh-elf-binutils Lephenixnoir/sh-elf-gcc
-giteapc install Lephenixnoir/OpenLibm Vhex-Kernel-Core/fxlibc Lephenixnoir/sh-elf-gcc
-giteapc install Lephenixnoir/gint Lephenixnoir/libprof
+
+bash giteapc-install.sh
+
+giteapc install -y Lephenixnoir/fxsdk@dev Lephenixnoir/sh-elf-binutils@dev Lephenixnoir/sh-elf-gcc@dev
+giteapc install -y Lephenixnoir/OpenLibm Vhex-Kernel-Core/fxlibc@dev Lephenixnoir/sh-elf-gcc@dev
+giteapc install -y Lephenixnoir/gint@dev Lephenixnoir/libprof
